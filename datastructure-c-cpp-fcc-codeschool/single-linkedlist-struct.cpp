36c5dcb12971eff41d8cb6e4c4f491dca4ec2b9a
Noor Mohammed <mail@noormohammed.tech>
2021-12-28 15:14:37 +0530

    linkedlist restructured for add between and makes interactive

--- datastructure-c-cpp-fcc-codeschool/single-linkedlist-struct.cpp
+++ datastructure-c-cpp-fcc-codeschool/single-linkedlist-struct.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-void insert(int data);
+void insert();
+void insertAt();
 void showNodes();
 
 struct Node
@@ -14,26 +15,73 @@ Node *tail = NULL;
 
 int main()
 {
-  int count, input;
-  cout << "enter no. of list : ";
-  cin >> count;
-  for (int i = 0; i < count; i++)
+  int i = 1, input, choice;
+  cout << "1.Append\n2.Insert(data, position)\n3.Show\n4.Exit\n";
+  while (choice != 4)
   {
-    cout << i + 1 << ". Enter no : ";
-    cin >> input;
-    insert(input);
-    showNodes();
+    cout << i << ". Enter enter choice : ";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+      insert();
+      break;
+    case 2:
+      insertAt();
+      break;
+    case 3:
+      showNodes();
+      break;
+    case 4:
+      cout << "Exit";
+      exit(0);
+      break;
+
+    default:
+      cout << "Invalid";
+      exit(0);
+    }
+    i++;
   }
   return 0;
 }
 
-void insert(int _data)
+void insert()
 {
+  int _data;
+  cout << "Enter Data : ";
+  cin >> _data;
   Node *newNode = new Node;
   newNode->data = _data;
   newNode->next = NULL;
   head == NULL ? head = tail = newNode : tail->next = newNode;
   tail = newNode;
+}
+void insertAt()
+{
+  int data, position;
+  cout << "Enter Data and position : ";
+  cin >> data >> position;
+  Node *newNode = new Node;
+  Node *Temp = head;
+  newNode->data = data;
+  newNode->next = NULL;
+  if (position <= 1)
+  {
+    newNode->next = head;
+    head = newNode;
+    return;
+  }
+  for (int i = 0; i < position - 2 && Temp->next != NULL; i++)
+  {
+    Temp = Temp->next;
+  }
+  if (Temp->next == NULL)
+  {
+    tail = newNode;
+  }
+  newNode->next = Temp->next;
+  Temp->next = newNode;
 }
 void showNodes()
 {
